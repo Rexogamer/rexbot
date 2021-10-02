e7ae0ca245f52363efb63375964e41a6dc0673e6
Ed L <beartechtalks@gmail.com>
2021-10-02 23:01:14 +0100

    feat: add usage for commands

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,32 +2,45 @@ import { Message } from "revolt.js/dist/maps/Messages";
 import { Command } from "../types/command";
 
 module.exports = {
-    name: 'help',
-    aliases: ['h'],
-    description: 'Get all available commands.',
-    developer: false,
+	name: "help",
+	aliases: ["h"],
+	description: "Returns a list of the bot's commands or, if a command is specified, info about the command.",
+	usage: "help [command]",
+	developer: false,
+	serverOnly: false,
 
-    run: async (msg: Message, args: string[]) => {
-        const input = args.join(' ');
-        let content = `## ${msg.client.user?.username} Help\n---\n`;
-        if (!input) {
-            // @ts-ignore
-            for (const cmd of msg.client.framework.commands) {
-                content += `- **${cmd.name}:** ${cmd.description || 'No description.'}\n`
-            }
-        } else {
-            // @ts-ignore
-            const cmd: Command = msg.client.framework.getCommand(input)
-            if (!cmd) {
-                content += "Nothing found!"
-            } else {
-                content += (
-                    `**${cmd.name}**\n${cmd.description || 'No description.'}\n`
-                    + `**Usage:** \`${(msg.content as string).substring(0, 1) + cmd.name}\`\n`
-                    + `**Aliases:** ${cmd.aliases.join(', ')}\n`
-                )
-            }
-        }
-        msg.channel?.sendMessage(content);
-    }
-}
+	run: async (msg: Message, args: string[]) => {
+		const input = args.join(" ");
+		// @ts-ignore
+		const authorIsDev = msg.client.framework.developers.includes(
+			msg.author_id
+		);
+		let content = `# ${msg.client.user?.username} Help\n`;
+		if (!input) {
+			// @ts-ignore
+			for (const cmd of msg.client.framework.commands) {
+				if (cmd.developer && !authorIsDev) continue;
+				content += `- **${cmd.name}:** ${
+					cmd.description || "No description."
+				}\n`;
+			}
+		} else {
+			// @ts-ignore
+			const cmd: Command = msg.client.framework.getCommand(input);
+			if (!cmd) {
+				content = "# Command not found\nThat doesn't seem to be a command - have you spelt the command's name correctly?";
+			} else {
+				content +=
+					`## ${cmd.name}\n${
+						cmd.description || "No description."
+					}\n` +
+					// @ts-ignore
+					`### Usage\n\`${msg.client.framework.prefix}${
+						cmd.usage || cmd.name
+					}\`\n` +
+					`### Aliases\n\`${cmd.aliases.join("`, `")}\``;
+			}
+		}
+		msg.channel?.sendMessage(content);
+	},
+};
